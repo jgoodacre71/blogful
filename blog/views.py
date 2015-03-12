dcf002dc8ac9d690f1baa5015436c9d62b5ce408
John Goodacre <jgoodacre71@gmail.com>
2015-03-12 12:56:14 +0000

    non working version...problem with db migration

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,13 @@ from .models import Post
 import mistune
 from flask import request, redirect, url_for
 
+from flask import flash
+from flask.ext.login import login_user
+from werkzeug.security import check_password_hash
+from .models import User
+from flask.ext.login import current_user
+from flask.ext.login import login_required
+
 
 
 @app.route("/")
@@ -36,22 +43,6 @@ def posts(page=1, paginate_by=10):
         total_pages=total_pages
     )
 
-
-@app.route("/post/add", methods=["GET"])
-def add_post_get():
-    return render_template("add_post.html")
-
-
-
-@app.route("/post/add", methods=["POST"])
-def add_post_post():
-    post = Post(
-        title=request.form["title"],
-        content=mistune.markdown(request.form["content"]),
-    )
-    session.add(post)
-    session.commit()
-    return redirect(url_for("posts"))
 
 @app.route("/post/<int:id>")
 def view_post(id):
@@ -83,3 +74,40 @@ def delete_post(id):
     session.delete(post)
     session.commit()
     return redirect(url_for("posts"))
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("posts"))
+
+
+
+@app.route("/post/add", methods=["GET"])
+@login_required
+def add_post_get():
+    return render_template("add_post.html")
+
+@app.route("/post/add", methods=["POST"])
+@login_required
+def add_post_post():
+    post = Post(
+        title=request.form["title"],
+        content=mistune.markdown(request.form["content"]),
+        author=current_user
+    )
+    session.add(post)
+    session.commit()
+    return redirect(url_for("posts"))
+
